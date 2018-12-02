a55da38f7b5cb30cc8904c9b6b91a596b9570ef7
Czarek Stu <czarstur@gmail.com>
2018-12-02 02:31:30 +0100

    Calculates how high Santa climbed and when did he went into basement

---
+++ santa2015.py
@@ -0,0 +1,22 @@
+pietro = 0
+n = 0
+with open('santa2015.txt', 'r') as f:
+    tekst = f.read()
+
+lista = [wyraz for wyraz in tekst]
+
+while pietro != -1:
+    for x in tekst:
+        if x == "(":
+            pietro += 1
+            n += 1
+            if pietro == -1:
+                break
+        elif x == ")":
+            pietro -= 1
+            n += 1
+            if pietro == -1:
+                break
+
+print(pietro)
+print(n)
